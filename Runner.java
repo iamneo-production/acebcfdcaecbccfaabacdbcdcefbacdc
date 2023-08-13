b81e62f5d344fba1a240aa82eafdc8b2e3d34b3f
Iamneo <techadmin@iamneo.ai>
2023-08-13 09:20:32 +0000

    Backup Commit

---
+++ Runner.java
@@ -0,0 +1,14 @@
+package Runner;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features= "features/OpenGoogle.feature" ,glue="stepDefinitions")
+public class RunnerClass {
+
+
+
+}
--- import io.cucumber.java.en.java
+++ import io.cucumber.java.en.java
@@ -1,42 +1,63 @@
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.When;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.And;
-import org.junit.Assert;
+package stepDefinitions;
 
-public class LoginSteps {
-    private String username;
-    private String password;
-    private String loginMessage;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.After;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+class OpenGoogleDefinitions
+{
+    private WebDriver driver;
 
     @Given("the user is on the Home page")
-    public void userIsOnHomePage() {
-        // Implementation to navigate to the Home page
+    public void openHomePage() {
+        System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
+        driver = new ChromeDriver();
+        driver.get("https://www.example.com");
     }
 
     @When("the user navigates to the Login page")
-    public void userNavigatesToLoginPage() {
-        // Implementation to navigate to the Login page
+    public void goToLoginPage() {
+        WebElement loginLink = driver.findElement(By.linkText("Login"));
+        loginLink.click();
     }
 
-    @And("the user enters username {string} and password {string}")
-    public void userEntersUsernameAndPassword(String username, String password) {
-        this.username = username;
-        this.password = password;
-        // Implementation to enter the username and password in the login form
+    @When("the user enters valid username and password")
+    public void enterValidCredentials() {
+        WebElement usernameField = driver.findElement(By.id("username"));
+        WebElement passwordField = driver.findElement(By.id("password"));
+        WebElement loginButton = driver.findElement(By.id("login-button"));
+
+        usernameField.sendKeys("valid_username");
+        passwordField.sendKeys("valid_password");
+        loginButton.click();
     }
 
     @Then("the successful login message is displayed")
-    public void successfulLoginMessageIsDisplayed() {
-        // Implementation to verify the successful login message
-        loginMessage = getLoginMessage();
+    public void checkSuccessfulLoginMessage() {
+        WebElement successMessage = driver.findElement(By.id("success-message"));
         String expectedMessage = "You have successfully logged into your account! There are multiple discount offers waiting for you!!";
-        Assert.assertEquals(expectedMessage, loginMessage);
+        String actualMessage = successMessage.getText();
+
+        assert actualMessage.equals(expectedMessage);
     }
 
-    // Helper method to retrieve the login message
-    private String getLoginMessage() {
-        // Implementation to retrieve the login message from the UI
-        return "You have successfully logged into your account! There are multiple discount offers waiting for you!!";
+    @Then("the error message {string} is displayed")
+    public void checkErrorMessage(String expectedErrorMessage) {
+        WebElement errorMessage = driver.findElement(By.id("error-message"));
+        String actualErrorMessage = errorMessage.getText();
+
+        assert actualErrorMessage.equals(expectedErrorMessage);
+    }
+
+    @After
+    public void closeBrowser() {
+        driver.quit();
     }
 }
